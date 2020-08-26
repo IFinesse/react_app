1be8431f0390b542ac48fb35615e210fc9569fa7
Ivan Shitikov <ifinesse2406@gmail.com>
2020-08-26 22:02:29 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            bbb<img src='https://placeit-assets1.s3-accelerate.amazonaws.com/custom-pages/make-a-gaming-logo/gaming-logo-maker-for-an-rpg-guild.png'></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://scitechdaily.com/images/New-Hubble-Image-Shows-Part-of-the-Large-Magellanic-Cloud.jpg'></img>
+            </div>
+            <div>
+                ava+desc
+        </div>
+            <div>
+                my posts
+            <div>
+                    New post
+            </div>
+                <div>
+                    <div>
+                        post1
+              </div>
+                    <div>
+                        post2
+              </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
